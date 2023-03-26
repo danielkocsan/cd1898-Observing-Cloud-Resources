fddf1c90f959b848f287d42ec0714a5289d03fe3
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2023-03-26 16:21:54 +0200

    - Add missing Alerting Rules and corresponding screenshots - Use EKS 1.25

--- starter/terraform/eks.tf
+++ starter/terraform/eks.tf
@@ -19,5 +19,5 @@ provider "kubernetes" {
    account            = data.aws_caller_identity.current.account_id
    private_subnet_ids = module.vpc.private_subnet_ids
    vpc_id             = module.vpc.vpc_id
-   k8s_version        = "1.24"
+   k8s_version        = "1.25"
  }
